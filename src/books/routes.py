22f441417061c6c6837bb57f4863bdf56576d568
fullstackyodha <harshal3196@gmail.com>
2024-10-06 18:49:30 +0530

    Added API Router

---
+++ src/books/routes.py
@@ -0,0 +1,69 @@
+from fastapi import status, APIRouter
+from fastapi.exceptions import HTTPException
+from typing import List
+from book_data import books
+from schemas import Book, BookUpdate
+from main import app
+
+
+book_router = APIRouter()
+
+
+# RETURNS LIST OF BOOKS
+@book_router.get("/books", response_model=List[Book])
+async def get_all_books():
+    return books
+
+
+@book_router.post("/book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_data: Book) -> dict:
+    # Generate a dictionary representation of the model
+    new_book = book_data.model_dump()
+    books.append(new_book)
+
+    return new_book
+
+
+@book_router.get("/book/{book_id}")
+async def get_book_by_id(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with {book_id} not found"
+    )
+
+
+@book_router.patch("/book/{book_id}")
+async def update_book_by_id(book_id: int, updated_book_data: BookUpdate) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = updated_book_data.title
+            book["author"] = updated_book_data.author
+            book["published_date"] = updated_book_data.published_date
+            book["page_count"] = updated_book_data.page_count
+            book["language"] = updated_book_data.language
+            return book
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with {book_id} not found"
+    )
+
+
+@book_router.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book_by_id(book_id: int):
+    book_to_delete = next((book for book in books if book["id"] == book_id), None)
+
+    if book_to_delete:
+        books.remove(book_to_delete)
+        return  # No content should be returned for status 204
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Book with id {book_id} not found",
+    )
+
+
+# Include an APIRouter in the same app
+app.include_router(book_router)
